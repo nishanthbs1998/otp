e676fb6fefc95875c0783fa56d7738d6c441bb61
Nishanth BS <nishanthbs18@gmail.com>
2023-05-03 12:08:43 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
+
 
 function App() {
+  const [value, setValue] = useState("");
+  const [dummyData, setDummyData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [recentId, setRecentId] = useState("");
+  const [timer, setTimer] = useState(0);
+  const [startTimer, setStartTimer] = useState(false);
+  const [btnD, setBtnD] = useState(true);
+  const [isMessage, setIsMessage] = useState(false);
+
+  const [showOtp, setShowOtp] = useState(false);
+  const intv = useRef(null);
+  useEffect(() => {
+    if (startTimer === false) return;
+    else {
+      if (timer < 30) {
+        intv.current = setInterval(() => {
+          setTimer((t) => t + 1);
+        }, 1000);
+      }
+    }
+    console.log("c");
+    return () => {
+      clearInterval(intv.current);
+    };
+  }, [startTimer, timer]);
+
+  useEffect(() => {
+    if (timer === 30) {
+      setBtnD(false);
+    } else {
+      setBtnD(true);
+    }
+  }, [timer]);
+
+  async function fetchData() {
+    setIsLoading(true);
+    const data = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const jsonData = await data.json();
+    setDummyData(jsonData);
+    setIsLoading(false);
+    setShowOtp(true);
+    setTimer(0);
+    setStartTimer(true);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (value === recentId) {
+      setValue("");
+      setIsMessage(true);
+      return;
+    }
+    clearInterval(intv.current);
+    setIsMessage(false);
+    setRecentId(value);
+    setValue("");
+    fetchData();
+    
+
+    
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="flex flex-col gap-10 m-10 justify-center items-center ">
+      <div className="flex flex-col gap-2">
+      <p className="font-semibold text-lg">Enter your Email</p>
+      <form onSubmit={handleSubmit} className="flex gap-2">
+        <input
+        className="border-2 border-gray-300 rounded-md p-1"
+          value={value}
+          type="email"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button type="submit" className="px-4 py-1 bg-orange-500 rounded-md text-white">Generate OTP</button>
+      </form>
+
+      </div>
+      
+      {isLoading && <p className="text-md">Please wait....</p>}
+      {isMessage && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Entered email is same as the previous email. Timer will not reset.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
+      {showOtp && (
+        <div className="flex flex-col gap-2 justify-center items-center">
+          <p className="font-medium">Enter OTP</p>
+          <input className="border-2 border-gray-300 rounded-md p-1"/>
+          <p>Resend OTP in {timer} sec(s)</p>
+          <button disabled={btnD} className={btnD?"py-1 px-4 rounded-md text-white bg-gray-300":"py-1 px-4 rounded-md text-white bg-gray-500"}>Resend</button>
+        </div>
+      )}
     </div>
   );
 }
